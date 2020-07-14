6724a94d3a6daf30b0916790e073c6ba2bef692d
Angelo Abdoelsamath <angelo.a@me.com>
2020-07-14 17:47:20 +0200

    Updates

---
+++ precache-manifest.f6f0dd7bf1289a87599f39d8099e809c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e4b0632acbaffadf0b5ac23ce7485d75",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c61549c29cc4eb879a9b",
+    "url": "/static/css/main.5c6afd9f.chunk.css"
+  },
+  {
+    "revision": "f5c98dc11d621cc0a090",
+    "url": "/static/js/2.7b12c823.chunk.js"
+  },
+  {
+    "revision": "5a98f98e29a6df7977c8f8d2444b57ac",
+    "url": "/static/js/2.7b12c823.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c61549c29cc4eb879a9b",
+    "url": "/static/js/main.d61cda27.chunk.js"
+  },
+  {
+    "revision": "2d12fbf19457e4720572",
+    "url": "/static/js/runtime-main.f2676984.js"
+  }
+]);
